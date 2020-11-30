8331af32399dd2686cdda44a648247b24e29a946
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2020-11-30 13:52:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,126 +2,217 @@
 let budgetInput = document.getElementById('budgetInput');
 let expenseName = document.getElementById('expenseName');
 let expenseInput = document.getElementById('expenseInput');
-let budgetAmount = document.getElementById('budgetAmount');
-let expenseAmount = document.getElementById('expenseAmount');
+let totalBudget = document.getElementById('totalBudget');
+let totalExpense = document.getElementById('totalExpense');
 let balance = document.getElementById('balance');
+let balancePar = document.getElementById('balancePar');
 let expenseNameOut = document.getElementById('expenseNameOut');
 let expenseQtyOut = document.getElementById('expenseQtyOut');
-let edit = document.getElementById('edit');
+let expenseButtons = document.getElementById('edit');
 let budgetWarning = document.getElementById('budgetWarning');
-let expensetWarning = document.getElementById('expenseWarning');
+let expenseWarning = document.getElementById('expenseWarning');
 const calculateBtn = document.getElementById('calculateBtn');
 const expenseBtn = document.getElementById('expenseBtn');
 
-let expenseNum = 0;
-let count = 0;
+let expenseObject = localStorage.hasOwnProperty('expenses') ? JSON.parse(localStorage['expenses']) : {};
+let keys = localStorage.hasOwnProperty('expenses') ? Math.max(...Object.keys(JSON.parse(localStorage['expenses']))) + 1 : 0;
+let id = keys === 0 ? 0 : keys;
+let budgetParams = {
+    'budget': 0,
+    'expenses': 0,
+    'balance': 0
+}
+
+//Classes
+class Expense {
+    constructor(name,amount) {
+        if(UI.checkInput(name, expenseWarning) || 
+        UI.checkInput(amount, expenseWarning)) {
+            throw new Error('Expense fields cannot be empty');
+        }
+        else {
+            this.name = name;
+            this.amount = amount;
+        }
+    }
+}
+
+class UI {
+    //Show all parameters on load from local storage
+    static showHTML() {
+        localStorage.getItem('budget') !== null ? budgetParams = JSON.parse(localStorage.getItem('budget')) : budgetParams;
+        totalBudget.textContent = budgetParams['budget'];
+        totalExpense.textContent = budgetParams['expenses'];
+        balance.textContent = budgetParams['balance'];
+        
+        Object.values(JSON.parse(localStorage['expenses'])).forEach(object => {
+            this.showExpense(object);
+        })
+    }
+
+    //Add exepnse input to budget object and show on total expense on page
+    addExpense(expenseObj, expenseOut) {
+        budgetParams['expenses'] += parseInt(expenseObj.amount); 
+        expenseOut.textContent = budgetParams['expenses'];
+    }
+
+    //Output expense name and amount on page
+    static showExpense(expenseObj) {
+        let html1= `
+        <div class="${expenseObj.id} expenseRow">
+            <p>${expenseObj.name}</p>
+        </div>
+        `;
+        let html2= `
+        <div class="${expenseObj.id} expenseRow">
+            <p>$<span class="${expenseObj.id}a">${expenseObj.amount}</span></p>
+        </div>
+        `;
+        let html3 = `
+            <div class="${expenseObj.id} expenseRow">
+                <button class="${expenseObj.id} btn editBtn" onclick="UI.editExpense(this)"><i class="fas fa-edit"></i></button>
+                <button class="${expenseObj.id} btn deleteBtn" onclick="UI.deleteExpense(this)"><i class="fas fa-trash"></i></button>
+            </div>
+        `;
+        
+        expenseNameOut.innerHTML += html1;
+        expenseQtyOut.innerHTML += html2;
+        expenseButtons.innerHTML += html3;
+    }
+
+    //Store object in local storage
+    storeExpense(expenseInstObj, expenseObj) {
+        expenseObj[`${id}`] = expenseInstObj;
+        expenseObj[`${id}`].id = id; 
+        localStorage.setItem('expenses', JSON.stringify(expenseObj));
+    }
+
+    //Add input value to budget object and output on the page
+    static enterBudget(inputVal, budget) {
+        if(this.checkInput(inputVal.value, budgetWarning)) {
+            throw new Error('Budget field cannot be empty');
+        }
+        else {
+            budgetParams['budget'] = inputVal.value;
+            budget.textContent = budgetParams['budget'];
+        }    
+    }
+
+    //Check if input is empty and show a warning if it is
+    static checkInput (input, warning) {
+        if(input === '') {
+            warning.style.display = 'inline-block';
+            setTimeout(() => {
+                warning.style.display = 'none';
+            }, 2000);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //Show balance on page and add it to the budget object
+    static showBalance(balanceP, balanceAmount) {
+        let difference = budgetParams['budget'] - budgetParams['expenses'];
+        budgetParams['balance'] = difference;
+
+        if(difference >= 0) {
+            balanceAmount.textContent = difference;
+            balanceP.style.color = 'rgb(19, 100, 53)';
+        }
+        else if(difference < 0) {
+            balanceAmount.textContent = difference;
+            balanceP.style.color = 'rgb(128, 18, 18)';
+        }
+    }
+
+    //Update local storage budget object
+    static updateBudget(budget) {
+        localStorage.setItem('budget', JSON.stringify(budget));
+    }
+
+    //Edit expense
+    static editExpense(e) {
+        let classList = e.classList[0];
+        let elements = document.getElementsByClassName(classList);
+        let name = elements[0].children[0];
+        let value = elements[1].children[0].children[0];
+        
+        expenseName.value = name.textContent; 
+        expenseInput.value = value.textContent;
+        this.deleteExpense(e); 
+    }
+
+    //Delete expense
+    static deleteExpense(e) {
+        let classList = e.classList[0];
+        let expAmount = document.getElementsByClassName(classList+'a')[0].textContent;
+        expAmount = Number(expAmount);
+        let elements = document.getElementsByClassName(classList);
+        for(let i = elements.length - 1; i >= 0; i--) {
+            elements[i].remove();
+        }
+
+        budgetParams['expenses'] -= expAmount;
+        totalExpense.textContent = budgetParams['expenses'];
+        
+        delete expenseObject[classList];
+        localStorage.setItem('expenses', JSON.stringify(expenseObject));
+        
+        this.showBalance(balancePar, balance); 
+    }
+
+    //Clear input fields
+    static clearFields(...fields) {
+        fields.forEach(field => {
+            field.value = '';
+        });
+    }
+}
 
 //Event listeners
-calculateBtn.addEventListener('click', enterBudget);
-expenseBtn.addEventListener('click', addExpense);
+document.addEventListener('DOMContentLoaded', () => {
+    UI.showHTML();
+});
 
-//Functions
-function enterBudget() {
-    let inputVal = budgetInput.value;
-    if(inputVal === '') {
-        showWarning(budgetWarning);
-    }
-    else {
-        budgetAmount.textContent = inputVal;
-        budgetInput.value = '';
-        showBalance();
-    }
-}
+calculateBtn.addEventListener('click', () => {
+    //Store budget input inside of budget params and show output
+    UI.enterBudget(budgetInput, totalBudget);
 
-//Adds the amount to total expense
-function addExpense() {
-    let expenseNameVal = expenseName.value;
-    let expenseVal = expenseInput.value;
-    if(expenseNameVal === '' || expenseVal === '') {
-        showWarning(expenseWarning);
-    }
-    else {
-        showExpense(expenseNameVal, expenseVal);
+    //Show balance
+    UI.showBalance(balancePar, balance);
 
-        expenseName.value = '';
-        expenseInput.value = '';
-        expenseNum += parseInt(expenseVal); 
-        expenseAmount.textContent = expenseNum;
-        showBalance();
-    }
-}
+    //Update budget object in local storage
+    UI.updateBudget(budgetParams);
 
-//Calculates the balance according to budget and total expense
-function showBalance() {
-    let balancePar = document.getElementById('balancePar');
-    let difference = budgetAmount.textContent- expenseAmount.textContent;
+    //Clear input field
+    UI.clearFields(budgetInput);
+});
+
+expenseBtn.addEventListener('click', () => {
+    //Instantiate expense class
+    let expense = new Expense(expenseName.value, expenseInput.value);
     
-    if(difference >= 0) {
-        balance.textContent = difference;
-        balancePar.style.color = 'rgb(19, 100, 53)';
-    }
-    else if(difference < 0) {
-        balance.textContent = difference;
-        balancePar.style.color = 'rgb(128, 18, 18)';
-    }
-}
+    //Instantiate ui class
+    let ui = new UI();
 
-//Outputs the name of the expense and the amount
-function showExpense(name, value) {
-    let html1= `
-    <div class="${count} expenseRow">
-        <p>${name}</p>
-    </div>
-    `;
-    let html2= `
-    <div class="${count} expenseRow">
-        <p>$<span class="${count}a">${value}</span></p>
-    </div>
-    `;
-    let html3 = `
-        <div class="${count} expenseRow">
-            <button class="${count} btn editBtn" onclick="editExp(this)"><i class="fas fa-edit"></i></button>
-            <button class="${count} btn deleteBtn" onclick="deleteExp(this)"><i class="fas fa-trash"></i></button>
-        </div>
-    `;
+    //Store expense object in local storage
+    ui.storeExpense(expense, expenseObject);
+
+    //Store expense input inside of budget params and show output
+    ui.addExpense(expense, totalExpense);
+
+    //Output expense name and amount
+    UI.showExpense(expense);
+    id++;
+
+    //Show balance
+    UI.showBalance(balancePar, balance);
+
+    //Update budget object in local storage
+    UI.updateBudget(budgetParams);
     
-    expenseNameOut.innerHTML += html1;
-    expenseQtyOut.innerHTML += html2;
-    edit.innerHTML += html3;
-    count++;
-}
-
-//Show warning if empty input fields
-function showWarning(warning) {
-    warning.style.display = 'block';
-    setTimeout(() => {
-        warning.style.display = 'none';
-    }, 4000);
-}
-
-//Edit one expense and update balance
-function editExp(e) {
-    let classList = e.classList[0];
-    let elements = document.getElementsByClassName(classList);
-    let name = elements[0].children[0];
-    let value = elements[1].children[0].children[0];
-    
-    expenseName.value = name.textContent; 
-    expenseInput.value = value.textContent;
-    deleteExp(e); 
-}
-
-//Delete one expense and update balance
-function deleteExp(e) { 
-    let classList = e.classList[0];
-    let expAmount = document.getElementsByClassName(classList+'a')[0].textContent;
-    expAmount = Number(expAmount);
-    let elements = document.getElementsByClassName(classList);
-    for(let i = elements.length - 1; i >= 0; i--) {
-        elements[i].remove();
-    }
-    
-    expenseNum -= expAmount;
-    expenseAmount.textContent = expenseNum;
-    showBalance(); 
-}
+    //Clear input fields
+    UI.clearFields(expenseName, expenseInput);
+});
